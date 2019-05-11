3bf475d0e5b1f0ab50d26d4c52065bc8d02f6fed
Rytis Kumpa <Rytis95@gmail.com>
2019-05-11 13:01:51 +0800

    Added inference

--- inference.py
+++ inference.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append('waveglow/')
+#sys.path.append('waveglow/')
 import numpy as np
 import torch
 import os
@@ -10,23 +10,23 @@ from layers import TacotronSTFT, STFT
 from audio_processing import griffin_lim
 from train import load_model
 from text import text_to_sequence
-from denoiser import Denoiser
+#from denoiser import Denoiser
 from scipy.io.wavfile import write
 
 hparams = create_hparams()
 hparams.sampling_rate = 22050
 
-checkpoint_path = "tacotron2_statedict.pt"
+checkpoint_path = "checkpoint_21000"
 model = load_model(hparams)
 model.load_state_dict(torch.load(checkpoint_path)['state_dict'])
 _ = model.cuda().eval().half()
 
-waveglow_path = 'waveglow_256channels.pt'
-waveglow = torch.load(waveglow_path)['model']
-waveglow.cuda().eval().half()
-for k in waveglow.convinv:
-    k.float()
-denoiser = Denoiser(waveglow)
+#waveglow_path = 'waveglow_256channels.pt'
+#waveglow = torch.load(waveglow_path)['model']
+#waveglow.cuda().eval().half()
+#for k in waveglow.convinv:
+#    k.float()
+#denoiser = Denoiser(waveglow)
 
 text = "Testuojame modelį!"
 sequence = np.array(text_to_sequence(text, ['basic_cleaners']))[None, :]
@@ -36,10 +36,12 @@ sequence = torch.autograd.Variable(
 mel_outputs, mel_outputs_postnet, _, alignments = model.inference(sequence)
 
 
-with torch.no_grad():
-    audio = waveglow.infer(mel_outputs_postnet, sigma=0.666)
+#with torch.no_grad():
+#    audio = waveglow.infer(mel_outputs_postnet, sigma=0.666)
 
-audio = denoiser(audio, strength=0.01)[:, 0]
+audio = griffin_lim(mel_outputs_postnet, TacotronSTFT)
+
+#audio = denoiser(audio, strength=0.01)[:, 0]
 
 audio = audio.squeeze()
 audio = audio.cpu().numpy()
