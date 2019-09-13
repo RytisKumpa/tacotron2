116b6fac2a2fe25cd106c925a73dd99f756caeee
Rytis Kumpa <Rytis95@gmail.com>
2019-09-13 20:22:19 +0100

    Infer and samples edited

--- hparams.py
+++ hparams.py
@@ -20,7 +20,6 @@ def create_hparams(hparams_string=None, verbose=False):
         cudnn_enabled=True,
         cudnn_benchmark=False,
         ignore_layers=['embedding.weight'],
-
         ################################
         # Data Parameters             #
         ################################
@@ -56,7 +55,7 @@ def create_hparams(hparams_string=None, verbose=False):
         n_frames_per_step=1,  # currently only 1 is supported
         decoder_rnn_dim=1024,
         prenet_dim=256,
-        max_decoder_steps=1000,
+        max_decoder_steps=4000,
         gate_threshold=0.5,
         p_attention_dropout=0.1,
         p_decoder_dropout=0.1,
@@ -78,7 +77,7 @@ def create_hparams(hparams_string=None, verbose=False):
         # Optimization Hyperparameters #
         ################################
         use_saved_learning_rate=False,
-        learning_rate=1e-3,
+        learning_rate=1e-4,
         weight_decay=1e-6,
         grad_clip_thresh=1.0,
         batch_size=64,
--- infer_waveglow.py
+++ infer_waveglow.py
@@ -16,12 +16,12 @@ from text import text_to_sequence
 hparams = create_hparams()
 hparams.sampling_rate = 22050
 sampling_rate = 22050
-checkpoint_path = "checkpoint_21000"
+checkpoint_path = "checkpoint_40000"
 model = load_model(hparams)
 model.load_state_dict(torch.load(checkpoint_path)['state_dict'])
 _ = model.cuda().eval().half()
 
-waveglow_path = 'waveglow_256channels.pt'
+waveglow_path = 'waveglow_20000'
 waveglow = torch.load(waveglow_path)['model']
 waveglow = waveglow.remove_weightnorm(waveglow)
 waveglow.cuda().eval().half()
@@ -29,22 +29,30 @@ for k in waveglow.convinv:
     k.float()
 #denoiser = Denoiser(waveglow)
 
+text = "Pasikiškiakopūsteliaudamasis. Nejaugi negalima greičiau ištarti?"
 text = "Šį modelį tik treniravau dvi dienas ir net nenaudojau visų duomenų. Kai sukarpysiu vusas audio knygas ir pabaigsiu treniravimą, nebus įmanoma atskirti šio kompiuteriu sugeneruoto įrašo nuo žmogaus!"
-sequence = np.array(text_to_sequence(text, ['basic_cleaners']))[None, :]
-sequence = torch.autograd.Variable(
-    torch.from_numpy(sequence)).cuda().long()
-	
-mel_outputs, mel_outputs_postnet, _, alignments = model.inference(sequence)
+text = "Dvaru vadinamą namą šalia Vijūnėlės tvenkinio Druskininkuose rentę statybininkai į žemę įgręžė aštuonis galingus polius, kad jo nesugriautų jokia gamtos stichija. Bet tvirtą statinį išjudino politiniai vėjai."
+text = "Vos pradėtas statyti pastatas tapo skundus rašančių vietos politikų taikiniu, o prieš savaitę, praėjus šešeriems metams nuo statybų pradžios, Aukščiausiasis teismas paskelbė galutinį sprendimą - jį būtina nugriauti."
 
-with torch.no_grad():
-    audio = 32768.0 * waveglow.infer(mel_outputs_postnet, sigma=0.666)[0]
-	
-	
-audio = audio.cpu().numpy()
-audio = audio.astype('int16')
-audio_path = os.path.join('samples', "{}_synthesis.wav".format('sample'))
-write(audio_path, sampling_rate, audio)
-print(audio_path)
+file = open("sample_text.txt", "r", encoding="utf-8")
+i = 0;
+for text in file:
+	sequence = np.array(text_to_sequence(text, ['basic_cleaners']))[None, :]
+	sequence = torch.autograd.Variable(
+		torch.from_numpy(sequence)).cuda().long()
+		
+	mel_outputs, mel_outputs_postnet, _, alignments = model.inference(sequence)
+
+	with torch.no_grad():
+		audio = 32768.0 * waveglow.infer(mel_outputs_postnet, sigma=0.666)[0]
+		
+		
+	audio = audio.cpu().numpy()
+	audio = audio.astype('int16')
+	audio_path = os.path.join('samples', "{}_synthesis.wav".format(i))
+	write(audio_path, sampling_rate, audio)
+	i += 1
+	print(audio_path)
 #audio_path = os.path.join('samples', "{}_synthesis.wav".format('sample'))
 #save_audio_to_file(audio, 22050, audio_path)
 
